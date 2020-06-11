73af051b33a333051ee75bfe5118559d950d7285
Michael Leeman <michaelpleeman@outlook.com>
2020-06-11 18:53:22 +0100

    Refactor URL hyperlink feature
    Refactor the newly added URL hyperlink feature to follow PEP8, write better comments and rename variables to better describe their purpose.

--- app/web_scraper.py
+++ app/web_scraper.py
@@ -27,15 +27,15 @@ hyperlink_list = []
 last_recent_date = datetime.strptime("19-05-2020", "%d-%m-%Y")
 
 
-# Extracts the job details and URL link from each job posting on the current page
+# Extracts the job details and hyperlink from each job posting on the current page
 def scrape_job_details(soup):
     for div in soup.find_all(name="div", attrs={"class": "job-listing mb-2"}):
         job_title = div.a["title"]
         company_name = div.find(name="span", attrs={"style": "display: ruby-base-container"}).string.split(None, 2)[1]
         job_type = div.find("span").string
         date_posted = div.find("span", attrs={"style": "order: 2"}).string.strip()
-        URL_hyperlink = div.a["href"]
-        hyperlink_list.append(URL_hyperlink)
+        job_hyperlink = div.a["href"]
+        hyperlink_list.append(job_hyperlink)
         job_list.append((job_title, company_name, job_type, date_posted))
     return job_list, hyperlink_list
 
@@ -60,19 +60,20 @@ def go_to_new_page():
 
 
 # Keep adding the jobs from the page until they are older than than "last_recent_date"
-def search_for_jobs(current_soup, job_list):
+def search_for_jobs(current_soup):
     keep_searching_for_jobs = True
     while keep_searching_for_jobs:
-        uncompleted_job_list, URL_list = scrape_job_details(current_soup)
-        job_list, keep_searching_for_jobs = remove_outdated_jobs(uncompleted_job_list, keep_searching_for_jobs)
-        t.sleep(1)
+        unsorted_job_list, URL_list = scrape_job_details(current_soup)
+        sorted_job_list, keep_searching_for_jobs = remove_outdated_jobs(unsorted_job_list, keep_searching_for_jobs)
+        t.sleep(0.5)
         current_soup = go_to_new_page()
-        t.sleep(1)
-    return job_list, URL_list
+        t.sleep(0.5)
+    return sorted_job_list, URL_list
 
 
-job_list, URL_list = search_for_jobs(soup, job_list)
+job_list, hyperlink_list = search_for_jobs(soup)
 driver.close()
+
 
 # -----------------------------------------------------------------------
 # Excel
@@ -111,8 +112,9 @@ def append_job_to_xl(job_list, worksheet):
     for job in job_list:
         worksheet.append(job)
         current_row = worksheet._current_row
-        worksheet["A" + str(current_row)].hyperlink = URL_list[URL_index]
+        worksheet["A" + str(current_row)].hyperlink = hyperlink_list[URL_index]
         URL_index += 1
+
 
 file_path = "Job_Openings.xlsx"
 book = Workbook()
