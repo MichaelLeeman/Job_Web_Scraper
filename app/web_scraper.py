cb02c9c0555325e49f96613b841f83becd12d886
Michael Leeman <michaelpleeman@outlook.com>
2020-06-13 13:43:01 +0100

    Reformat date posted
    Reformat the job's posted date from numbering the month to month name (e.g. "19-05-2020" to "19-May-2020"). Also, add new comment describe the new more_job_details() function and other unclear lines of code.

--- app/web_scraper.py
+++ app/web_scraper.py
@@ -24,43 +24,48 @@ driver.find_element_by_link_text('Close').click()
 
 job_list = []
 hyperlink_list = []
-last_recent_date = datetime.strptime("19-05-2020", "%d-%m-%Y")
+last_recent_date = datetime.strptime("19-May-2020", "%d-%b-%Y")
 
 
 # Extracts the job details and hyperlink from each job posting on the current page
 def scrape_job_details(soup):
     for div in soup.find_all(name="div", attrs={"class": "job-listing mb-2"}):
+        job_hyperlink = div.a["href"]
         job_title = div.a["title"]
         company_name = div.find(name="span", attrs={"style": "display: ruby-base-container"}).string.split(None, 2)[1]
         job_type = div.find("span").string
-        date_posted = div.find("span", attrs={"style": "order: 2"}).string.strip()
-        job_hyperlink = div.a["href"]
+
+        # Format the date posted to match other dates
+        unformatted_date = div.find("span", attrs={"style": "order: 2"}).string.strip()
+        date_posted = datetime.strptime(unformatted_date, "%d-%m-%Y").strftime('%d-%b-%Y')
 
         expiry_date = more_job_details(job_hyperlink)
-        print(expiry_date)
+
         hyperlink_list.append(job_hyperlink)
         job_list.append((job_title, company_name, job_type, date_posted, expiry_date))
     return job_list, hyperlink_list
 
 
-# Scrapes for extra job details in the job's web page.
+# Scrapes for extra job details found inside the job's web page.
 def more_job_details(job_hyperlink):
     driver.get(job_hyperlink)
     current_page = requests.get(driver.current_url)
     new_soup = BeautifulSoup(current_page.text, "html.parser")
-
-    date_list = new_soup.find("small").string.split()[8: 11]
+    t.sleep(0.5)
+    # Deadline is written in a string text. To extract the date, the text is converted to a list.
+    date_text_into_list = new_soup.find("small").string.split()[8: 11]
     separator = '-'
-    expiry_date = separator.join(date_list)
+    expiry_date = separator.join(date_text_into_list)   # Joining only the day, month and year back into a string
     driver.back()
+    t.sleep(0.5)
     return expiry_date
 
 
-# Checks whether the date posted of every job and removes it from the list if it's too old
-# Returns a boolean to stop searching for jobs once they are old ones
+# Checks the date posted of every job and removes it if it's too old
+# Returns a boolean to stop searching for jobs on the next pages
 def remove_outdated_jobs(job_list, keep_searching):
     for job in job_list[:]:
-        job_datetime = datetime.strptime(job[3], "%d-%m-%Y")
+        job_datetime = datetime.strptime(job[3], '%d-%b-%Y')    # Needs to convert back to datetime to make comparison
         if job_datetime < last_recent_date:
             job_list.remove(job)
             keep_searching = False
@@ -111,12 +116,12 @@ def setup_worksheet(worksheet):
     for column_cell in worksheet.columns:
         max_char_len = 0
         for cell in column_cell:
-            if max_char_len < len(cell.value):
-                max_char_len = len(cell.value)
-        new_column_length = max_char_len * 0.95
+            if max_char_len < len(str(cell.value)):
+                max_char_len = len(str(cell.value))
+        new_column_length = max_char_len
         worksheet.column_dimensions[column_cell[0].column_letter].width = new_column_length
 
-    # Colours every other row blue
+    # Colours every other row light blue
     for every_other_row in range(3, worksheet.max_row + 1, 2):
         for cell in worksheet[every_other_row]:
             cell.fill = PatternFill(start_color="BBDEFB", fill_type="solid")
@@ -128,6 +133,7 @@ def append_job_to_xl(job_list, worksheet):
     for job in job_list:
         worksheet.append(job)
         current_row = worksheet._current_row
+        # Adds a hyperlink to each job web page in the job title column
         worksheet["A" + str(current_row)].hyperlink = hyperlink_list[URL_index]
         URL_index += 1
 
