485b91279d539a0a7ab9405e718053926c8113b7
Michael Leeman <michaelpleeman@outlook.com>
2020-07-07 21:57:50 +0100

    Add load workbook feature
    Add a feature that loads the workbook from the excel directory if it already exists. If not, then it creates a new workbook.
    
    To avoid the same jobs being appended to workbook again, the append_jobs_to_xl() functions checks whether each scraped job is already in the workbook before appending it. Once it finds a job that is already in the worksheet, then it stops appending jobs.
    
    Also, rename some functions to not conflict with the openpyxl methods and to better describe their purpose.

--- app/excel.py
+++ app/excel.py
@@ -2,13 +2,12 @@
 
 from openpyxl.styles import Font, PatternFill
 from openpyxl import Workbook
+from openpyxl import load_workbook
 
 
 # setups the worksheet by appending the data and calling the other styling functions
-def setup_worksheet(worksheet, job_list, hyperlink_list, company_link_list, table_headers):
-    worksheet.append(table_headers)
-    create_table_headers(worksheet, font_colour="FFFFFF", cell_colour="2196F3")
-    append_job_to_xl(job_list, hyperlink_list, company_link_list, worksheet)
+def setup_xlsx(worksheet, job_list, hyperlink_list, company_link_list):
+    append_jobs_to_xl(job_list, hyperlink_list, company_link_list, worksheet)
     autofit_columns(worksheet)
     colour_rows(worksheet, colour="BBDEFB")
     filter_and_freeze_panes(worksheet)
@@ -16,6 +15,9 @@ def setup_worksheet(worksheet, job_list, hyperlink_list, company_link_list, tabl
 
 # Stylise the titles
 def create_table_headers(worksheet, font_colour, cell_colour):
+    table_headers = ("Job Openings", "Company", "Job Type", "Date Posted", "Deadline", "Salary Range")
+    worksheet.append(table_headers)
+
     for column_title in worksheet[1:1]:
         column_title.font = Font(bold=True, color=font_colour)
         column_title.fill = PatternFill(start_color=cell_colour, fill_type="solid")
@@ -34,6 +36,10 @@ def autofit_columns(worksheet):
 
 # Colours every other row with the colour parameter
 def colour_rows(worksheet, colour):
+    for every_other_row in range(2, worksheet.max_row + 1):
+        for cell in worksheet[every_other_row]:
+            cell.fill = PatternFill(start_color="FFFFFF", fill_type="solid")
+
     for every_other_row in range(3, worksheet.max_row + 1, 2):
         for cell in worksheet[every_other_row]:
             cell.fill = PatternFill(start_color=colour, fill_type="solid")
@@ -48,26 +54,44 @@ def filter_and_freeze_panes(worksheet):
 
 
 # Appends each job opening to the worksheet and creates a hyperlink to its page
-def append_job_to_xl(job_list, hyperlink_list, company_link_list, worksheet):
-    URL_index = 0
+def append_jobs_to_xl(job_list, hyperlink_list, company_link_list, worksheet):
+    URL_index, first_xl_job = 0, tuple(cell.value for cell in worksheet[2:2])
+
+    # Fixes bug where a blank row is appended
+    if worksheet["A2"].value is None:
+        worksheet.delete_rows(2)
+
+    # Only append jobs that are not in the worksheet. This is done by checking over an iteration whether the first job
+    # in the worksheet is the same as the current job in the job list.
     for job in job_list:
-        worksheet.append(job)
-        current_row = worksheet._current_row
-        # Adds a hyperlink to each job web page in the job title column
-        worksheet["A" + str(current_row)].hyperlink = hyperlink_list[URL_index]
-        if company_link_list[URL_index] is not None:
-            worksheet["B" + str(current_row)].hyperlink = company_link_list[URL_index]
-        URL_index += 1
+        if job != first_xl_job:
+            worksheet.append(job)
+            current_row = worksheet._current_row
+            # Adds a hyperlink to each job web page in the job title column
+            worksheet["A" + str(current_row)].hyperlink = hyperlink_list[URL_index]
+            if company_link_list[URL_index] is not None:
+                worksheet["B" + str(current_row)].hyperlink = company_link_list[URL_index]
+            URL_index += 1
+        else:
+            break
 
 
-# Initialise the workbook and worksheet
-def init_workbook(worksheet_title):
+# Initialise a new workbook and worksheet
+def init_xlsx(worksheet_title):
     workbook = Workbook()
     worksheet = workbook.active
     worksheet.title = worksheet_title
+    create_table_headers(worksheet, font_colour="FFFFFF", cell_colour="2196F3")
+    return workbook, worksheet
+
+
+# Loads an existing workbook
+def load_xlsx(file_path):
+    workbook = load_workbook(filename=file_path)
+    worksheet = workbook.active
     return workbook, worksheet
 
 
 # Saves the workbook
-def save_workbook(workbook, file_path):
+def save_xlsx(workbook, file_path):
     workbook.save(file_path)
--- app/main.py
+++ app/main.py
@@ -14,15 +14,21 @@ driver.get(URL)
 driver.find_element_by_link_text('Close').click()
 
 current_date = datetime.today()
-date_fortnight_ago = current_date - timedelta(weeks=2)
+date_fortnight_ago = current_date - timedelta(days=1)
 last_recent_date = date_fortnight_ago.replace(hour=0, minute=0, second=0, microsecond=0)  # default to midnight
 
 job_list, hyperlink_list, company_link_list = [], [], []
 job_list, hyperlink_list, company_link_list = web_scraper.search_for_jobs(soup, last_recent_date, driver)
 driver.close()
 
-workbook, worksheet = excel.init_workbook(worksheet_title="Job Openings")
-table_headers = ("Job Openings", "Company", "Job Type", "Date Posted", "Deadline", "Salary Range")
-excel.setup_worksheet(worksheet, job_list, hyperlink_list, company_link_list, table_headers)
 file_path = os.path.abspath("main.py").rstrip('/app/main.py') + '//Workbooks' + "//Job_Openings.xlsx"
-excel.save_workbook(workbook, file_path)
+
+# If the Job_Openings workbook already exists then append the jobs
+# If not, create a new workbook and then append the jobs
+if os.path.isfile(file_path):
+    workbook, worksheet = excel.load_xlsx(file_path)
+else:
+    workbook, worksheet = excel.init_xlsx(worksheet_title="Job Openings")
+
+excel.setup_xlsx(worksheet, job_list, hyperlink_list, company_link_list)
+excel.save_xlsx(workbook, file_path)
